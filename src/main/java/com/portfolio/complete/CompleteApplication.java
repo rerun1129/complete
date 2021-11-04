7fe2a688b3fbe57e9799d25c043e365fa8b6f05b
rerun1129 <84177337+rerun1129@users.noreply.github.com>
2021-11-05 08:13:37 +0900

    1105 프로젝트 시작

---
+++ src/main/java/com/portfolio/complete/CompleteApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.complete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompleteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/complete/CompleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.complete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
