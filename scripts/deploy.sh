cd39728806ad54f8a02d27564dad64f3239e2c96
rerun1129 <84177337+rerun1129@users.noreply.github.com>
2021-11-08 20:35:32 +0900

    1108 20시 프로젝트 이름 갈았음

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 REPOSITORY=/home/ec2-user/app/step2
-PROJECT_NAME=complete
+PROJECT_NAME=gt-webserver
 
 echo "> 빌드 파일 복사"
 
@@ -9,7 +9,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl complete | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl gt-webserver | grep jar | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
