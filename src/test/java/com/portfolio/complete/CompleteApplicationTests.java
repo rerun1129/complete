63d04c012ea33a43824a280bb98aa06de56ac22f
rerun1129 <84177337+rerun1129@users.noreply.github.com>
2021-11-08 12:15:33 +0900

    1108 12시 travis 푸시

--- src/test/java/com/portfolio/complete/CompleteApplicationTests.java
+++ src/test/java/com/portfolio/complete/CompleteApplicationTests.java
@@ -1,9 +1,11 @@
 package com.portfolio.complete;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class CompleteApplicationTests {
 
 	@Test
