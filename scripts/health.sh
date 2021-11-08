405f8741c67eb1a59a1d5fab62226ba445984f5c
rerun1129 <84177337+rerun1129@users.noreply.github.com>
2021-11-09 02:26:02 +0900

    1109 02시 초기 설정 프로젝트 최종 배포

--- scripts/health.sh
+++ scripts/health.sh
@@ -5,7 +5,7 @@ ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 source ${ABSDIR}/switch.sh
 
-IDLE_PORT=${find_idle_port}
+IDLE_PORT=$(find_idle_port)
 
 echo "> Health Check Start!"
 echo "> IDLE_PORT: $IDLE_PORT"
@@ -19,21 +19,21 @@ do
 
   if [ ${UP_COUNT} -ge 1 ]
   then # $up_count >= 1 ("real" 문자열이 있는지 검증)
-      echo "> Health Check 성공"
+      echo "> Health check 성공"
       switch_proxy
       break
   else
-    echo "> Health Check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
-    echo "> Health Check: ${RESPONSE}"
+      echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
+      echo "> Health check: ${RESPONSE}"
   fi
 
   if [ ${RETRY_COUNT} -eq 10 ]
   then
-    echo "> Health Check 실패"
+    echo "> Health check 실패. "
     echo "> 엔진엑스에 연결하지 않고 배포를 종료합니다."
     exit 1
   fi
 
-  echo "> Health Check 연결 실패. 재시도..."
+  echo "> Health check 연결 실패. 재시도..."
   sleep 10
 done
